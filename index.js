01a9c130dd9abc4789ebce2b68794025f55102ee
Jacob Stephens <jacobstephens2@gmail.com>
2021-03-02 10:25:04 -0800

    added heroku build script

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 
 const server = require("./api/server");
-// const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-server.listen(2000, () => {
-  console.log(`Server running on port: ${2000}`);
+server.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
 });
